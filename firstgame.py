ea7d8cf1ca690f045f96e3e8ea6538bac578d0c1
neerajkr007 <44816075+neerajkr007@users.noreply.github.com>
2019-07-25 20:14:48 +0530

    ship game
    trying to make a spaceship game

--- firstgame.py
+++ firstgame.py
@@ -9,24 +9,31 @@ win = pygame.display.set_mode((screenheight, screenWidth))
 
 pygame.display.set_caption('First Game', 'test')
 
-velocity = 5
+font = pygame.font.SysFont(None, 20)
+
+velocity = 3
 width = 40
 height = 60
-x = random.randrange(0, screenWidth - width - 10, 10)
-y = random.randrange(0, screenheight - height - 10, 10)
+x = 250
+y = 440
 isJump = False
 jumpCount = 10
+backGround = pygame.image.load("bg.jpg").convert()
+shipImg = pygame.image.load("shipImg.png").convert()
+
+clock = pygame.time.Clock()
 
 run = True
 while run:
-    pygame.time.delay(50)
+    pygame.time.delay(16)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    clock.tick(60)
+    fps = int(clock.get_fps())
     keys = pygame.key.get_pressed()
-
     if keys[pygame.K_a] and x > velocity - 5:
         x -= velocity
     if keys[pygame.K_d] and x < screenWidth - width:
@@ -44,15 +51,25 @@ while run:
             if jumpCount < 0:
                 neg = -1
             y -= ((jumpCount ** 2) / 2) * neg
-            print(jumpCount)
             jumpCount -= 1
         else:
             isJump = False
             jumpCount = 10
 
     win.fill((0, 0, 0))
-
+    win.blit(backGround, [0, 0])
     pygame.draw.rect(win, (0, 191, 255), (x, y, width, height))
+    rect = shipImg.get_rect()
+    rect.center = (200, 300)
+    showFps = font.render('FPS ' + str(fps), True, (255, 0, 0))
+    win.blit(showFps, [0, 0])
+    win.blit(shipImg, [x, y])
     pygame.display.update()
 
 pygame.quit()
+
+
+
+
+
+
