612e52449f78276087e4a12695673b02fb33d7f4
neerajkr007 <44816075+neerajkr007@users.noreply.github.com>
2019-07-26 19:46:56 +0530

    finished
    maybe add a few more features later

--- firstgame.py
+++ firstgame.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,7 +8,7 @@ pygame.init()
 screenWidth = 500
 screenheight = 500
 win = pygame.display.set_mode((screenheight, screenWidth))
-pygame.display.set_caption('First Game', 'test')
+pygame.display.set_caption('Space Bakchodi', 'test')
 
 # font
 
@@ -36,13 +37,33 @@ class Bullet(object):
         self.y = playerShip.y
         self.radius = radius
         self.color = color
-        self.vel = 3
+        self.vel = 7
 
     def draw(self, win):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+        #enemyship = pygame.image.load("enemy.png")
+        #circle = enemyship.get_circle()
+        #circle.center = (self.x, 30)
+        #win.blit(enemyship, [self.x, self.y])
 
 
 bullets = []
+
+enemyship = pygame.image.load("enemy.png")
+enemyship = pygame.transform.rotate(enemyship, 180)
+
+class Enemy(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 2
+
+    def draw(self, win):
+        pygame.draw.rect(win, (0, 255, 0), (self.x, self.y, self.width, self.height), -2)
+        win.blit(enemyship, [self.x - 12, self.y])
+
 
 clock = pygame.time.Clock()
 
@@ -52,25 +73,38 @@ backGround = pygame.image.load("bg.jpg").convert()
 
 def screendraw(win):
     win.blit(backGround, [0, 0])
-    Fps(win)
+    Dashboard(win)
     playerShip.draw_ship(win)
     for bullet in bullets:
         bullet.draw(win)
+    for enemy in enemies:
+        enemy.draw(win)
     pygame.display.update()
 
 
-def Fps(win):
+def Dashboard(win):
     showFps = font.render('FPS ' + str(fps), True, (255, 0, 0))
     win.blit(showFps, [0, 0])
+    showpoints = font.render('Score = ' + str(score), True, (255, 0, 0))
+    win.blit(showpoints, [410, 0])
+    enemypass = font.render('enemies pass :  ' + str(points), True, (255, 0, 0))
+    win.blit(enemypass, [350, 400])
+
 
 firetime = 0
+enemytime = 0
+score = 0
+points = 0
+
+enemies = []
+
 run = True
 playerShip = Ship(250, 440, 40, 50)
-
+collision = False
 # main loop
 
 while run:
-    pygame.time.delay(16)
+    pygame.time.delay(0)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -82,11 +116,29 @@ while run:
         else:
             bullets.pop(bullets.index(bullet))
 
+    for enemy in enemies:
+        if enemy.y < screenheight:
+            enemy.y += enemy.vel
+        else:
+            enemies.pop(enemies.index(enemy))
+            points += 1
+
+    for enemy in enemies:
+        for bullet in bullets:
+            if (bullet.x >= enemy.x) and (bullet.x <= (enemy.x + enemy.width)) and (bullet.y <= enemy.y + enemy.height) and (bullet.y >= enemy.y):
+                enemies.pop(enemies.index(enemy))
+                score += 1
+
+    # collision = (bullet.x >= enemy.x) and (bullet.x <= (enemy.x + enemy.width))
     clock.tick(60)
     if firetime > 0:
         firetime += 1
     if firetime >= 15:
         firetime = 0
+    if enemytime > 0:
+        enemytime += 1
+    if enemytime >= 100:
+        enemytime = 0
     fps = int(clock.get_fps())
     keys = pygame.key.get_pressed()
     if keys[pygame.K_a] and playerShip.x > playerShip.velocity_ship - 5:
@@ -99,10 +151,15 @@ while run:
         playerShip.y += playerShip.velocity_ship
     v = playerShip.y
     if keys[pygame.K_SPACE] and firetime == 0:
-        if len(bullets) < 6:
-            bullets.append(Bullet(round(playerShip.x + playerShip.width //2), round(playerShip.y), 6, (255, 0, 0)))
+        if len(bullets) < 3:
+            bullets.append(Bullet(round(playerShip.x + playerShip.width //2), round(playerShip.y), 4, (255, 0, 0)))
         firetime = 1
 
+    if enemytime == 0:
+        enemies.append((Enemy(random.randrange(0, screenWidth - 40, 10), 0, 40, 60)))
+        enemytime = 1
     screendraw(win)
+    if(points == 10):
+        run = False
     # win.fill((0, 0, 0))
 pygame.quit()
