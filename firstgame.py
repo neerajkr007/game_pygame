657282d487eb33dc26ee2a6a944eefbc62d5724a
neerajkr007 <44816075+neerajkr007@users.noreply.github.com>
2019-07-26 11:49:11 +0530

    Add files via upload

--- firstgame.py
+++ firstgame.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 
 pygame.init()
 
@@ -12,12 +11,11 @@ pygame.display.set_caption('First Game', 'test')
 font = pygame.font.SysFont(None, 20)
 
 velocity = 3
+velocity_bullet = 1
 width = 40
 height = 60
 x = 250
 y = 440
-isJump = False
-jumpCount = 10
 backGround = pygame.image.load("bg.jpg").convert()
 shipImg = pygame.image.load("shipImg.png").convert()
 
@@ -38,33 +36,27 @@ while run:
         x -= velocity
     if keys[pygame.K_d] and x < screenWidth - width:
         x += velocity
-    if not isJump:
-        if keys[pygame.K_w] and y > velocity - 5:
-            y -= velocity
-        if keys[pygame.K_s] and y < screenheight - height:
-            y += velocity
-        if keys[pygame.K_SPACE]:
-            isJump = True
-    else:
-        if jumpCount >= -10:
-            neg = 1
-            if jumpCount < 0:
-                neg = -1
-            y -= ((jumpCount ** 2) / 2) * neg
-            jumpCount -= 1
-        else:
-            isJump = False
-            jumpCount = 10
+    if keys[pygame.K_w] and y > velocity - 5:
+        y -= velocity
+    if keys[pygame.K_s] and y < screenheight - height:
+        y += velocity
+    v = y
+    if event.type == pygame.KEYUP or event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_SPACE:
+            while v > 0:
+                v -= velocity_bullet
 
     win.fill((0, 0, 0))
     win.blit(backGround, [0, 0])
-    pygame.draw.rect(win, (0, 191, 255), (x, y, width, height))
+    bullet = pygame.draw.rect(win, (255, 0, 0), (x + 20, v, 10, 10))
+    ship = pygame.draw.rect(win, (0, 191, 255), (x, y, width, height))
     rect = shipImg.get_rect()
     rect.center = (200, 300)
     showFps = font.render('FPS ' + str(fps), True, (255, 0, 0))
     win.blit(showFps, [0, 0])
     win.blit(shipImg, [x, y])
     pygame.display.update()
+
 
 pygame.quit()
 
